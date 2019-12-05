a13320376ad4e81190e0d9aa6d65da76cf368cd3
Administrator <Administrator@MSDN-SPECIAL>
2019-12-05 11:07:29 +0900

    [sksksksdkfaksdf]

---
+++ team_project/src/test/java/kr/or/ksmart/team_project/TeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart.team_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
