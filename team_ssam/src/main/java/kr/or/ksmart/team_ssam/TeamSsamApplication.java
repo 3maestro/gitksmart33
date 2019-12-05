b5d8ab79b23d86220fabff1bb4c3750ff60c8d21
Administrator <Administrator@MSDN-SPECIAL>
2019-12-05 11:03:51 +0900

    [team1] teamssam

---
+++ team_ssam/src/main/java/kr/or/ksmart/team_ssam/TeamSsamApplication.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart.team_ssam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamSsamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamSsamApplication.class, args);
+	}
+
+}
---
+++ team_ssam/src/test/java/kr/or/ksmart/team_ssam/TeamSsamApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart.team_ssam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamSsamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
