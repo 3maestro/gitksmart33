3d195ec2d10c71d0e08cf63f8d6719fe4a4874bd
Administrator <Administrator@MSDN-SPECIAL>
2019-12-05 10:59:07 +0900

    [team1] project genius

---
+++ team_github/src/main/java/kr/or/ksmart/team_github/TeamGithubApplication.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart.team_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamGithubApplication.class, args);
+	}
+
+}
